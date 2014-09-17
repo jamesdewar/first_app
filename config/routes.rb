d011f9bd430fd6fcdcad8b1501b1818134a8e932
jamesdewar <james.dewar@ividata.com>
2014-09-17 11:26:49 +0200

    finnished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'static_pages/home'
+  root 'static_pages#home'
 
   get 'static_pages/help'
 
-  resources :microposts
-  resources :users
-  root 'users#index'
+  get 'static_pages/about'
+
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
